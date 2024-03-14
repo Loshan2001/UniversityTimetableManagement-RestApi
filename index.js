e42a474845f6260b9c31d2eea305b487d8476cec
loshan <it21803970@my.sliit.lk>
2024-03-14 15:03:53 +0530

    build a connection with mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const url = 'mongodb+srv://loshansp2001:Lo$h%40n200111@afbackend.nazhnpm.mongodb.net/TimetableManagement'
 
+//connect the database 
+mongoose.connect(url,{useNewUrlParser : true}) 
+const con = mongoose.connection 
+con.on('open',()=>{
+    console.log('mongoDB connected.....')
+})
 
 app.listen(8080,()=>{
     console.log("server running on port 8080.....")
-})
+})    
